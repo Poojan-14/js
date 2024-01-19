246c65c07b1e7e125dcdd19aab91505a38efde0a
POOJAN <129759039+Poojan-14@users.noreply.github.com>
2024-01-19 14:37:30 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,3 +2,25 @@
 
 // alert(3 + 3) // we are using node.js, not browser.
 
+console.log(3
+    + 
+    3)    // code readability should be high
+
+console.log("poojan")
+
+let name = "poojan"
+let age = 19 
+let isLoggedIn = false
+
+// number => 2 power of 53 {range}
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined

1fe4cdf368bd96f247c4862c513557f73b583025
POOJAN <129759039+Poojan-14@users.noreply.github.com>
2024-02-02 13:55:14 +0000

    conversion operatins

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,60 @@ let booleanIsLoggdIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/**********************************************oprations**********************************************/
+
+let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 % 2);
+// console.log(2 / 2);
+
+let str1 = "hello";
+let str2 = " poojan"
+
+let str3 = str1 + str2 ;
+
+// console.log(str3);
+
+const gameName = new String("poojan-pd");
+const newString = gameName.slice(-7, 4)
+// console.log(newString)
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getMonth() + 1 );
+
+// const myArr = [0, 1, 2, 3, 4, 5]
+const myArr = new Array(0,1,2,3,4,5);
+
+// console.log("A ",myArr);
+
+let myn1 = myArr.slice(1,3);
+// console.log(myn1);
+// console.log("b ", myArr);
+
+let myn2 = myArr.splice(1,3);
+// console.log(myn2);
+// console.log("c ", myArr)
+
+const mysym = Symbol('mykey')
+
+const jsUser = {
+    name : "poojan",
+    age : 19,
+    email : "poojan@google.com",
+    [mysym] : "key1"
+
+}
+
+// console.log(jsUser)
+
+jsuser
